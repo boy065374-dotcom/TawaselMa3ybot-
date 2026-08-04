4804d7b29033705b33b358c59e77c192c7f36792
boy065374-dotcom <boy065374@gmail.com>
2026-08-04 18:13:08 +0300

    Update send.py

--- commands/send.py
+++ commands/send.py
@@ -1,23 +1,34 @@
+import os
+from dotenv import load_dotenv
+
 from telegram import Update
 from telegram.ext import ContextTypes, ConversationHandler
 
+load_dotenv()
+
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+
 MESSAGE = 1
+
 
 async def send_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         "✍️ اكتب الرسالة التي تريد إرسالها للمطور:"
     )
+
     return MESSAGE
 
 
-async def receive_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    admin_id = 123456789  # حط ID المطور هنا
+async def receive_send_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.message.from_user
 
     await context.bot.send_message(
-        chat_id=admin_id,
+        chat_id=ADMIN_ID,
         text=(
-            "📩 رسالة جديدة من مستخدم:\n\n"
-            f"{update.message.text}"
+            "📩 رسالة جديدة للمطور:\n\n"
+            f"👤 الاسم: {user.first_name}\n"
+            f"🆔 ID: {user.id}\n\n"
+            f"💬 الرسالة:\n{update.message.text}"
         )
     )
 
