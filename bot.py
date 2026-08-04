ec4003fe8f5d734b864033b4847940097bbca8ab
boy065374-dotcom <boy065374@gmail.com>
2026-08-04 18:02:11 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,32 @@
+import os
+from dotenv import load_dotenv
 
+from telegram.ext import (
+    Application,
+    CommandHandler
+)
+
+from handlers.start import start
+from commands.help import help_command
+from commands.about import about_command
+
+
+load_dotenv()
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("help", help_command))
+    app.add_handler(CommandHandler("about", about_command))
+
+    print("Bot is running...")
+
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    main()
